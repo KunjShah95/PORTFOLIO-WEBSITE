ce09b5627c3f0c0eaeaa66c4d8d4d3022801a109
KunjShah95 <kkshah05072005@gmail.com>
2024-12-12 10:36:21 +0530

    project

--- project/src/js/skills.js
+++ project/src/js/skills.js
@@ -21,7 +21,7 @@ export const skills = [
     },
     {
         title: 'Programming Languages',
-        description: 'C, C++, Java, Python',
+        description: 'C, C++, Java, Python, Go',
         icon: `<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4"/>`
-    }
+    },
 ];
